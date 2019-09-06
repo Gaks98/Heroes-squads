f44da9a636583dfa38dbaadbcf1e6e27285d23a7
Gaks98 <yvonnegax98@gmail.com>
2019-09-06 17:46:12 +0300

    provided the methods i'll use in SquadDao interface

--- src/main/java/dao/SquadDao.java
+++ src/main/java/dao/SquadDao.java
@@ -1,4 +1,25 @@
 package dao;
 
+import models.Squad;
+import models.Hero;
+import java.util.List;
+
 public interface SquadDao {
+
+        //LIST
+        List<Squad> getAll();
+
+        //CREATE
+        void add (Squad category);
+
+        //READ
+        Squad findById(int id);
+
+        //UPDATE
+        void update(int id, String name);
+
+        //DELETE
+        void deleteById(int id);
+        void clearAllSquads();
+
 }
