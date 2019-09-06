b4277dab9029cfe9ae0b4b8bbac1f0699fc90cfc
Gaks98 <yvonnegax98@gmail.com>
2019-09-06 16:12:33 +0300

    added update and delete in the Sql2oHeroDao class and ofcourse first in the HeroDao interface

--- src/main/java/dao/HeroDao.java
+++ src/main/java/dao/HeroDao.java
@@ -14,10 +14,10 @@ public interface HeroDao {
     //READ
     Hero findById(int id);
 
-    // UPDATE
-    // void update(int id, String content);
+     //UPDATE
+     void update(int id, String content);
 
-    // DELETE
-    // void deleteById(int id);
-    // void clearAllTasks();
+     //DELETE
+     void deleteById(int id);
+     void clearAllTasks();
 }
--- src/main/java/dao/Sql2oHeroDao.java
+++ src/main/java/dao/Sql2oHeroDao.java
@@ -51,4 +51,35 @@ public class Sql2oHeroDao implements HeroDao {
             return con.createQuery("SELECT * FROM heroes WHERE id = :id").addParameter("id", id).executeAndFetchFirst(Hero.class); //fetch an individual item
         }
     }
+
+    @Override
+    public void update(int id, String newDescription){
+        String sql = "UPDATE heroes SET description = :description WHERE id=:id";
+        try (Connection con = sql2o.open()){
+            con.createQuery(sql).addParameter("description",newDescription).addParameter("id",id).executeUpdate();
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void deleteById(int id){
+        String sql = "DELETE from heroes WHERE id=:id";
+        try (Connection con = sql2o.open()){
+            con.createQuery(sql).addParameter("id",id).executeUpdate();
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void clearAllHeroes() {
+        String sql = "DELETE from heroes";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
 }
