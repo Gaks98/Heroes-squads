6dd6b848b438e4e98d5b8a0d3652434463eb2ae6
Gaks98 <yvonnegax98@gmail.com>
2019-09-06 14:22:44 +0300

    try to open connection

--- src/main/java/dao/Sql2oHeroDao.java
+++ src/main/java/dao/Sql2oHeroDao.java
@@ -1,5 +1,7 @@
 package dao;
 
+import models.Hero;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 public class Sql2oHeroDao implements HeroDao {
@@ -7,5 +9,14 @@ public class Sql2oHeroDao implements HeroDao {
 
     public Sql2oHeroDao(Sql2o sql2o) {
         this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Hero hero){
+        String sql = "INSERT INTO heroes (description) VALUES (:description)";
+
+        try (Connection con = sql2o.open()){
+            
+        }
     }
 }
