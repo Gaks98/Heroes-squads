0e0d65065f382ecff97eac3f9d3e1f36ee9cf363
Gaks98 <yvonnegax98@gmail.com>
2019-09-06 12:15:37 +0300

    created a helper method that creates a new hero for us meaning incase of wanting to make a change will only do it at one place

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,18 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
 
+    
+
+
+
+
+    //HELPER METHOD
+    public Hero setupNewHero(){
+        return new Hero("Hulk");
+    }
 }
