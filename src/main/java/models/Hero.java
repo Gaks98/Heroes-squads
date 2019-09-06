488530fcdea5ee5fc6ef0d7c37d71ab6f83f3aa4
Gaks98 <yvonnegax98@gmail.com>
2019-09-06 12:01:46 +0300

    added sql20 dependencies

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,30 @@
 package models;
 
 public class Hero {
+    private String description;
+    private boolean assigned;
+    private int id;
+
+    public Hero(String description){
+        this.description = description;
+        this.assigned =false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAssigned(boolean assigned) {
+        this.assigned = assigned;
+    }
+
+    //getter
+    public boolean isAssigned() {
+        return assigned;
+    }
+
 }
