e9f274b5598ee376c7f25385d65d6cc85a39b005
Gaks98 <yvonnegax98@gmail.com>
2019-09-06 16:45:31 +0300

    update, deleteById and clearAll tests pass

--- src/test/java/dao/Sql2oHeroDaoTest.java
+++ src/test/java/dao/Sql2oHeroDaoTest.java
@@ -43,16 +43,48 @@ public class Sql2oHeroDaoTest {
     }
     
     @Test
-    public void GetAlletrievesAllHeroes_2(){
+    public void GetAllRetrievesAllHeroes_2() throws Exception{
         Hero hero1 = new Hero("Hulk");
         Hero hero2 = new Hero("superman");
         heroDao.add(hero1);
         heroDao.add(hero2);
-        assertEquals(2,heroDao.getAll());
+        assertEquals(2,heroDao.getAll().size());
     }
 
     @Test
-    public void NoHeroesAreFoundIfThereIsNone_0() {
-        assertEquals(0,heroDao.getAll());
+    public void NoHeroesAreFoundIfThereIsNone_0() throws Exception {
+        assertEquals(0,heroDao.getAll().size());
+    }
+
+    @Test
+    public void update_changesHeroesDetail() throws Exception{
+        String initialDescription = "Hulk";
+        Hero hero = new Hero(initialDescription);
+        heroDao.add(hero);
+        heroDao.update(hero.getId(),"wonder woman");
+        Hero updatedHero = heroDao.findById(hero.getId());
+        assertNotEquals(initialDescription,updatedHero.getDescription());
+    }
+
+    @Test
+    public void deleteById_deletesCorrectHero_0() throws Exception {
+        Hero hero = new Hero("Hulk");
+        heroDao.add(hero);
+        heroDao.deleteById(hero.getId());
+        assertEquals(0,heroDao.getAll().size());
+    }
+
+    @Test
+    public void clearAll_clearsAll() {
+        Hero hero = new Hero("Hulk");
+        Hero otherHero = new Hero("wonder woman");
+        heroDao.add(hero);
+        heroDao.add(otherHero);
+        //will hold the number of heroes before they are all cleared
+        int daoSize = heroDao.getAll().size();
+        heroDao.clearAllHeroes();
+        //daoSize is still equal to 2 while heroDao.getAll().size() is equal to zero
+        //daoSize is not deleted after the clearAllHeroes() because it's not part of it.
+        assertTrue(daoSize > 0 && daoSize > heroDao.getAll().size());
     }
 }
