6aa6c87e80a9d8a8e4efcbfc2dc05c9cd3bc6fe0
Gaks98 <yvonnegax98@gmail.com>
2019-09-06 12:45:30 +0300

    created 2 fields sql2o class connection and another field of type Sql2oHeroDao

--- src/test/java/dao/Sql2oHeroDaoTest.java
+++ src/test/java/dao/Sql2oHeroDaoTest.java
@@ -1,4 +1,13 @@
 package dao;
 
+import models.Hero;
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class Sql2oHeroDaoTest {
+    private Sql2oHeroDao heroDao;
+    private Connection conn;
+
+
 }
